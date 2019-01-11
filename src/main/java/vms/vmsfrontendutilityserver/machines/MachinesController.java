5d642805b27beefa513d9dc4d6a47e08437e2a6c
Viktor Doroshenko <viktordoroshenko@Viktors-MacBook-Air.local>
2019-01-11 15:08:16 +0200

    final

--- src/main/java/vms/vmsfrontendutilityserver/machines/MachinesController.java
+++ src/main/java/vms/vmsfrontendutilityserver/machines/MachinesController.java
@@ -16,40 +16,41 @@ import vms.vmsfrontendutilityserver.dto.OperationStatusEnum;
 import vms.vmsfrontendutilityserver.dto.machines.MachineDTO;
 import vms.vmsfrontendutilityserver.dto.machines.MachineStateDTO;
 
-
 @RestController
 @RequestMapping(ApiConstants.MACHINES)
 public class MachinesController {
 
   @Autowired
   IMachines machines;
-  
+
   @PostMapping(ApiConstants.ADD_MACHINE)
   public OperationStatusEnum addMachine(@RequestBody MachineDTO machine) {
-     return machines.addMachine(machine);
+    return machines.addMachine(machine);
   }
-  
+
   @PostMapping(ApiConstants.UPDATE_MACHINE)
   public OperationStatusEnum updateMachine(@RequestBody MachineDTO machine) {
-	  return machines.updateMachine(machine);
+    return machines.updateMachine(machine);
   }
-  
+
   @GetMapping(ApiConstants.GET_MACHINE + "/{machine_id}")
   public MachineDTO getMachine(@PathVariable("machine_id") int machineId) {
-	  return machines.getMachine(machineId);
+    return machines.getMachine(machineId);
   }
+
   @DeleteMapping(ApiConstants.REMOVE_MACHINE + "/{machine_id}")
   public OperationStatusEnum removeMachine(@PathVariable("machine_id") int machineId) {
-	  return machines.removeMachine(machineId);
+    return machines.removeMachine(machineId);
   }
+
   @GetMapping(ApiConstants.GET_MACHINE_STATE + "/{machine_id}")
   public MachineStateDTO getMachineState(@PathVariable("machine_id") int machineId) {
-	   return machines.getMachineState(machineId);
-	  }
-  @GetMapping(ApiConstants.GET_MACHINE_ALL)
-  public List<MachineDTO> getAllMachine(){
-	  return machines.getAllMachines();
+    return machines.getMachineState(machineId);
   }
- 
+
+  @GetMapping(ApiConstants.GET_MACHINE_ALL)
+  public List<MachineDTO> getAllMachine() {
+    return machines.getAllMachines();
+  }
 
 }
